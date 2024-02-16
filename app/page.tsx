26a073c15b93aecfce6bcd2f10f6b1786ea53f5f
Rohan2104 <maindarkarrohan@yahoo.com>
2024-02-16 18:15:57 +0530

    added some files and folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-export default function Home() {
+import React from "react";
+
+const Home = () => {
   return (
     <h1 className="p-5 m-10 text-3xl  font-bold underline">Hello world!</h1>
   );
-}
+};
+
+export default Home;
